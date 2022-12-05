0283ac624e192baee2e08d2b7a5d86557bc02e82
Wesley Ho <drakonkinstmc@gmail.com>
2022-12-04 20:51:16 -0800

    Remove comments

--- Day4/puzzle1.py
+++ Day4/puzzle1.py
@@ -7,13 +7,11 @@ def main():
         items = line.split(",")
         pair1 = items[0].split("-")
         pair2 = items[1].split("-")
-        #print(pair1)
-        #print(pair2)
         a = int(pair1[0])
         b = int(pair1[1])
         c = int(pair2[0])
         d = int(pair2[1])
-        #print(a, b, c, d)
+        
         if a <= c and d <= b:
             total += 1
         elif c <= a and b <= d:
--- Day4/puzzle2.py
+++ Day4/puzzle2.py
@@ -7,13 +7,11 @@ def main():
         items = line.split(",")
         pair1 = items[0].split("-")
         pair2 = items[1].split("-")
-        #print(pair1)
-        #print(pair2)
         a = int(pair1[0])
         b = int(pair1[1])
         c = int(pair2[0])
         d = int(pair2[1])
-        #print(a, b, c, d)
+        
         if (a <= c <= b) or (a <= d <= b) or (c <= a <= d) or (c <= b <= d):
             total += 1
     print(total)
