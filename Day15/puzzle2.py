ffbdfe488cd37a39cedac2181b9ce5a09fae871e
Wesley Ho <drakonkinstmc@gmail.com>
2022-12-14 23:51:11 -0800

    Polish and comment day 15 solution

--- Day15/puzzle2.py
+++ Day15/puzzle2.py
@@ -21,12 +21,14 @@ def main():
         sensors.append((sX, sY, bX, bY))
             
     for y in range(width + 1):
-        #print("Y", y)
+        # Create list of intervals for this value of Y only
         intervals = []
         for sX, sY, bX, bY in sensors:
             dX = abs(sX - bX)
             dY = abs(sY - bY)
             dist = dX + dY
+            # Calculate how long the interval must be at this Y value, possible
+            # since this is manhattan distance
             intervalAtY = dist - abs(sY - y)
             
             if intervalAtY < 0:
@@ -35,30 +37,41 @@ def main():
             minX = sX - intervalAtY
             maxX = sX + intervalAtY
             intervals.append((minX, maxX))
+        # Sort intervals - critical for compression algorithm so it knows it can move on
+        # Sorts by minX first, then maxX. minX sorting is the most important here
+        # Since it is used to determine if a new interval should start
         intervals.sort()
         
+        # Interval compression algorithm
         q = []
         for minX, maxX in intervals:
+            # No other intervals, add
             if len(q) <= 0:
                 q.append([minX, maxX])
                 continue
+
             currMin = q[-1][0]
             currMax = q[-1][1]
+            
+            # Check if this can extend the current interval
+            # Or if it needs to be a new one
             if minX > currMax + 1:
                 q.append([minX, maxX])
                 continue
+            # Extend current interval
             q[-1][1] = max(currMax, maxX)
         
         x = 0
         for minX, maxX in q:
-            #print("X", x)
             if x < minX:
                 print("ANS", x * width + y)
                 return
             
-            x = max(x, maxX + 1)
-            if x >= width + 1:
+            x = maxX + 1
+            if x > width:
                 break
+        
+        # Show progress
         if y % every == 0:
             print(y)
 
