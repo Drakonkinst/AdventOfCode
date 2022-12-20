847899016d0af40b85175bf985dbdd75be674be6
Wesley Ho <drakonkinstmc@gmail.com>
2022-12-19 22:52:02 -0800

    Add day 20

---
+++ Day20/puzzle2.py
@@ -0,0 +1,72 @@
+from collections import deque
+
+KEY = 811589153
+
+def main():
+    file = open("input.txt", "r");
+    lines = [line.strip() for line in file.readlines()]
+    
+    original = []
+    copy = []
+    lookup = {}
+    freq = {}
+    n = 0
+    for line in lines:
+        val = int(line) * KEY
+        if val in freq:
+            freq[val] += 1
+        else:
+            freq[val] = 1
+        x = (val, freq[val])
+        original.append(x)
+        copy.append(x)
+        lookup[x] = n
+        n += 1
+    
+    numMix = 10
+    for m in range(numMix):
+        for i in range(n):
+            #print(copy)
+            toMove = original[i]
+            val = toMove[0]
+            
+            if val % n == 0:
+                continue
+
+            toMoveIndex = lookup[toMove]
+            timesCircled = (toMoveIndex + val) // n
+            # What if adding this timesCircled offset makes it overflow?
+            # While it's inconsistent, keep adjusting the value until it is
+            while (toMoveIndex + val + timesCircled) // n != timesCircled:
+                timesCircled = (toMoveIndex + val + timesCircled) // n
+            newIndex = (toMoveIndex + val + timesCircled) % n
+            
+            if newIndex == toMoveIndex:
+                continue
+
+            step = 1 if newIndex > toMoveIndex else -1
+            for j in range(toMoveIndex, newIndex, step):
+                movingVal = copy[j + step]
+                copy[j] = movingVal
+                lookup[movingVal] = j
+            copy[newIndex] = toMove
+            lookup[toMove] = newIndex
+
+        zeroIndex = lookup[(0, 1)]
+        n1 = copy[(zeroIndex + 1000) % n][0]
+        n2 = copy[(zeroIndex + 2000) % n][0]
+        n3 = copy[(zeroIndex + 3000) % n][0]
+        #print([x[0] for x in copy])
+        print(m + 1, n1, n2, n3, "TOTAL", n1 + n2 + n3)
+        print()
+        
+        # If we're changing the instruction order
+        # original = copy
+        # copy = []
+        # lookup = {}
+        # for i in range(n):
+        #     copy.append(original[i])
+        #     lookup[original[i]] = i
+
+if __name__ == "__main__":
+    main()
