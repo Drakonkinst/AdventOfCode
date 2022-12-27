cd0864af682077dd1d54f6dac2eb8deb4d9a53c1
Wesley Ho <drakonkinstmc@gmail.com>
2022-12-26 20:30:25 -0800

    Add 2015 day 4

---
+++ 2015/Day4/puzzle1.py
@@ -0,0 +1,22 @@
+import sys, os, re, itertools
+from collections import deque
+from sortedcontainers import SortedDict, SortedList
+from utils import *
+import hashlib
+
+file = open("input.txt", "r")
+lines = [line.strip() for line in file.readlines()]
+
+def main():
+    n = 1
+    line = lines[0]
+    while True:
+        s = line + str(n)
+        h = hashlib.md5(s.encode()).hexdigest()
+        if h[:5] == "00000":
+            print(n)
+            return
+        n += 1
+
+if __name__ == "__main__":
+    main()
