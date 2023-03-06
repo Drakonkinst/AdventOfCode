f012694c801ca76101a553baf0987821b8e953cb
Wesley Ho <drakonkinstmc@gmail.com>
2023-03-05 22:52:38 -0800

    Add 2017 day 5

---
+++ 2017/Day5/puzzle2.py
@@ -0,0 +1,25 @@
+import sys, os, re, math, itertools as itt
+from collections import deque, Counter
+from sortedcontainers import SortedDict, SortedList
+from utils import *
+
+file = open("input.txt", "r")
+lines = [line.strip() for line in file.readlines()]
+
+def main():
+    jumps = [int(line) for line in lines]
+    jumpIndex = 0
+    step = 0
+    
+    while 0 <= jumpIndex < len(jumps):
+        jumpDistance = jumps[jumpIndex]
+        if jumpDistance >= 3:
+            jumps[jumpIndex] -= 1
+        else:
+            jumps[jumpIndex] += 1
+        jumpIndex += jumpDistance
+        step += 1
+    print(step)
+
+if __name__ == "__main__":
+    main()
