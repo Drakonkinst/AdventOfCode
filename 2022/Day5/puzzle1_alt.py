c25c5c586ec3b704d1871091b049252631f58480
Drakonkinst <drakonkinstmc@gmail.com>
2023-11-26 22:07:31 -0800

    Add additional solution to 2022 day 5

---
+++ 2022/Day5/puzzle1_alt.py
@@ -0,0 +1,47 @@
+import sys, os, re, math, itertools as itt
+from collections import deque, Counter
+from sortedcontainers import SortedDict, SortedList
+from utils import *
+
+file = open("input.txt", "r")
+lines = [line for line in file.readlines()]
+
+def main():
+    i = 0
+    width = 0
+    data = []
+    
+    # Read data
+    while i < len(lines):
+        line = lines[i]
+        if line[1].isnumeric():
+            break
+        row = []
+        rowWidth = 0
+        for letter in line[1::4]:
+            row.append(letter)
+            rowWidth += 1
+        width = max(width, rowWidth)
+        data.append(row)
+        i += 1
+    
+    # Parse data
+    blocks = [[] for _ in range(width)]
+    for row in data[::-1]:
+        for index, item in enumerate(row):
+            if item != " ":
+                blocks[index].append(item)
+    
+    # Do instructions
+    i += 2
+    while i < len(lines):
+        a, x, y = ints(lines[i])
+        for _ in range(a):
+            item = blocks[x - 1].pop()
+            blocks[y - 1].append(item)
+        i += 1
+    
+    print("".join([b[-1] for b in blocks]))
+
+if __name__ == "__main__":
+    main()
