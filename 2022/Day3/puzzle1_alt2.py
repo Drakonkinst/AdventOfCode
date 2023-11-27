831a8e39486e99e55894db34c34ab19d464ac6f9
Drakonkinst <drakonkinstmc@gmail.com>
2023-11-26 21:23:16 -0800

    Add additional solution to 2022 day 3

--- 2022/Day3/puzzle1_alt2.py
+++ 2022/Day3/puzzle1_alt2.py
@@ -6,10 +6,19 @@ from utils import *
 file = open("input.txt", "r")
 lines = [line.strip() for line in file.readlines()]
 
+PRIO = LOWERCASE_STR + UPPERCASE_STR
+
 def main():
     total = 0
     for line in lines:
-        pass
+        mid = len(line) // 2
+        first = line[:mid]
+        second = line[mid:]
+        first_c = set([c for c in first])
+        for c in second:
+            if c in first_c:
+                total += PRIO.index(c) + 1
+                break
     print(total)
 
 if __name__ == "__main__":
--- 2022/Day3/puzzle2_alt2.py
+++ 2022/Day3/puzzle2_alt2.py
@@ -6,10 +6,16 @@ from utils import *
 file = open("input.txt", "r")
 lines = [line.strip() for line in file.readlines()]
 
+PRIO = LOWERCASE_STR + UPPERCASE_STR
+
 def main():
     total = 0
-    for line in lines:
-        pass
+    for l1, l2, l3 in grouped(lines, 3):
+        l1_c = set([c for c in l1])
+        l2_c = set([c for c in l2])
+        l3_c = set([c for c in l3])
+        same = list(set.intersection(l1_c, l2_c, l3_c))
+        total += PRIO.index(same[0]) + 1
     print(total)
 
 if __name__ == "__main__":
