ddbb1e81a4798e7f687fadfc5d47316f3af2a7ce
Drakonkinst <drakonkinstmc@gmail.com>
2023-11-30 20:36:22 -0800

    Add alternate solution for 2017 day 6

---
+++ 2017/Day6/puzzle1_alt.py
@@ -0,0 +1,39 @@
+import sys, os, re, math, itertools as itt
+from collections import deque, Counter
+from sortedcontainers import SortedDict, SortedList
+from utils import *
+
+file = open("input.txt", "r")
+lines = [line.strip() for line in file.readlines()]
+blocks = ints(lines[0])
+
+def do_op():
+    maxIndex, maxValue = max_index(blocks)
+    toAll = maxValue // len(blocks)
+    blocks[maxIndex] = 0
+    for i in range(len(blocks)):
+        blocks[i] += toAll
+
+    remaining = maxValue % len(blocks)
+    index = maxIndex
+    while remaining > 0:
+        index = (index + 1) % len(blocks)
+        blocks[index] += 1
+        remaining -= 1
+
+def main():
+    step = 0    
+    visited = set()
+    visited.add(tuple(blocks))
+    
+    while True:
+        step += 1
+        do_op()
+        t = tuple(blocks)
+        if t in visited:
+            break
+        visited.add(t)
+    print(step)
+
+if __name__ == "__main__":
+    main()
