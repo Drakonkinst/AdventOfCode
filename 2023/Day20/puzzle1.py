4362aca9f7099f92f1dd764d05194138749da7da
Drakonkinst <drakonkinstmc@gmail.com>
2023-12-23 15:38:57 -0800

    Add 2023 day 20

---
+++ 2023/Day20/puzzle1.py
@@ -0,0 +1,143 @@
+import sys, os, itertools as itt
+from collections import deque # append(), pop(), popleft()
+from queue import PriorityQueue # put(), get()
+from sortedcontainers import SortedDict, SortedList
+from utils import *
+from functools import cache
+
+file = open("input.txt", "r")
+lines = [line.strip() for line in file.readlines()]
+
+class Module:
+    def is_state_reset(self):
+        return True
+    
+    # Return True to send high pulse, False to send low pulse, or None to send no pulse
+    def receive_pulse(self, name, isHigh):
+        return isHigh
+
+class FlipFlop(Module):
+    
+    def __init__(self):
+        self.state = False
+    
+    def is_state_reset(self):
+        return self.state == False
+    
+    def receive_pulse(self, name, isHigh):
+        if (isHigh):
+            return None
+        self.state = not self.state
+        return self.state
+    
+class Conjunction(Module):
+    
+    def __init__(self):
+        self.inputs = {}
+    
+    def set_inputs(self, inputs):
+        self.inputs = {}
+        for inputName in inputs:
+            self.inputs[inputName] = False
+    
+    def is_state_reset(self):
+        return all(memory == False for memory in self.inputs.values())
+    
+    def receive_pulse(self, name, isHigh):
+        self.inputs[name] = isHigh
+        if all(memory == True for memory in self.inputs.values()):
+            return False
+        return True
+    
+def main():
+    nodes = {}
+    inputs = {}
+    outputs = {}
+    
+    # Parse input into modules
+    for line in lines:
+        signature, outputStr = [s.strip() for s in line.split("->")]
+        name = None
+        if signature.startswith("%"):
+            name = signature[1:]
+            nodes[name] = FlipFlop()
+        elif signature.startswith("&"):
+            name = signature[1:]
+            nodes[name] = Conjunction()
+        else:
+            name = signature
+            # Default module acts as a broadcaster
+            nodes[signature] = Module()
+        
+        # In case no outputs
+        if not outputStr:
+            continue
+        
+        outputs[name] = outputStr.split(", ")
+        for output in outputs[name]:
+            if output not in inputs:
+                inputs[output] = []
+            inputs[output].append(name)
+    
+    # Retroactively set inputs now that all modules have been parsed
+    for name, module in nodes.items():
+        if isinstance(module, Conjunction):
+            #print("Module", name, "has", len(inputs[name]), "inputs:", inputs[name])
+            module.set_inputs(inputs[name])
+    
+    numLow = 0
+    numHigh = 0
+    maxPushes = 1000
+    pushCount = 0
+    
+    while pushCount < maxPushes:
+        pushCount += 1
+        # Push the button!
+        pulses = deque()
+        pulses.append(("broadcaster", "button", False))
+        while pulses:
+            (receiver, sender, isHigh) = pulses.popleft()
+            #print(sender, "-low->" if not isHigh else "-high->", receiver)
+            if isHigh:
+                numHigh += 1
+            else:
+                numLow += 1
+            if receiver not in nodes:
+                continue
+            module = nodes[receiver]
+            result = module.receive_pulse(sender, isHigh)
+            if result is not None:
+                for output in outputs[receiver]:
+                    pulses.append((output, receiver, result))
+        if all(node.is_state_reset() for node in nodes.values()):
+            break
+    
+    #print("CYCLE FOUND", pushCount, numHigh, numLow)
+    pushesRemaining = maxPushes - pushCount
+    if pushesRemaining > 0:
+        numCycles = pushesRemaining // pushCount
+        numLow *= (numCycles + 1)
+        numHigh *= (numCycles + 1)
+        pushesRemaining = pushesRemaining - (numCycles * pushCount)
+        #print("CYCLE COMPUTED", numCycles, pushesRemaining)
+    
+    while pushesRemaining > 0:
+        pushesRemaining -= 1
+        pulses = deque()
+        pulses.append(("broadcaster", "button", False))
+        while pulses:
+            (receiver, sender, isHigh) = pulses.popleft()
+            if isHigh:
+                numHigh += 1
+            else:
+                numLow += 1
+            module = nodes[receiver]
+            result = module.receive_pulse(sender, isHigh)
+            if result is not None:
+                for output in outputs[receiver]:
+                    pulses.append((output, receiver, result))
+    #print(numLow, numHigh, numLow * numHigh)
+    print(numLow * numHigh)
+
+if __name__ == "__main__":
+    main()
