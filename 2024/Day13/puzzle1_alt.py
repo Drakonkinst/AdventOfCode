dbb5ab0f4db7de310fb7ae996c7afe44f0f61f5c
Drakonkinst <drakonkinstmc@gmail.com>
2024-12-19 23:07:53 -0800

    Add 2024 day 13

--- 2024/Day13/puzzle1_alt.py
+++ 2024/Day13/puzzle1_alt.py
@@ -11,7 +11,6 @@ lines = [line.strip() for line in file.readlines()]
 
 COST_A = 3
 COST_B = 1
-BONUS = 10000000000000
 
 def main():
     total = 0
@@ -19,16 +18,11 @@ def main():
         ax, ay = ints(lines[i])
         bx, by = ints(lines[i + 1])
         gx, gy = ints(lines[i + 2])
-        gx += BONUS
-        gy += BONUS
         solution = find_solution(ax, ay, bx, by, gx, gy)
-        print(ax, ay, bx, by, gx, gy, "->", solution)
         if solution is not None:
             solution_a, solution_b = solution
             cost = get_cost(solution_a, solution_b)
             total += cost
-    # 74609225581053 is too high
-    assert total < 74609225581053
     print(total)
 
 def get_cost(a, b):
@@ -36,19 +30,13 @@ def get_cost(a, b):
 
 def find_solution(ax, ay, bx, by, gx, gy):
     # Algebra go brrrrr
-    assert (ay * bx - ax * by) != 0 # No parallel lines pls
     b = (ay * gx - ax * gy) / (ay * bx - ax * by)
-    # No decimals or negative numbers pls
-    if int(b) != b or b < 0:
+    if int(b) != b:
         return None
     # Just to make sure
     a1 = (gx - b * bx) / ax
     a2 = (gy - b * by) / ay
-    # Pls
     assert a1 == a2
-    # No decimals or negative numbers pls
-    if int(a1) != a1 or a1 < 0:
-        return None
     return int(a1), int(b)
 
 if __name__ == "__main__":
